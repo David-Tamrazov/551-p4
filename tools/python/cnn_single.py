33069911b170ba65b53709b738b1852aea322728
haoxu <haoxuw@gmail.com>
2017-11-28 00:25:10 -0500

    Initial version of multi-task cnn

--- tools/python/cnn_single.py
+++ tools/python/cnn_single.py
@@ -1,10 +1,7 @@
-#sudo pip install h5py
-
 import keras
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.models import model_from_json
 from keras.layers import Dense, Activation, Conv2D, Flatten
 from keras.optimizers import Adam as Adam
 from keras.callbacks import LearningRateScheduler
@@ -18,21 +15,17 @@ fmnist_test_path = '../../data/fashion_ocv/fashion_train.ocv'
 mnist_train_path = '../../data/mnist_ocv/mnist_train.ocv'
 mnist_test_path = '../../data/mnist_ocv/mnist_test.ocv'
 
-# filepath for the pre-trained multitask model
-serialized_multitask_model_path = "./multitask_model.json"
-serialized_multitask_weights_path = "./multitask_weights.h5"
+
 # hyper parameters 
 IMAGE_SIZE = 28
-EPOCHS = 5 #todo 50
+EPOCHS = 50
 LEARNING_RATE = 1e-3
 BATCH_SIZE = 100
 
 
 # function to fetch data - if testing = true, it'll fetch the entire dataset. otherwise it'll load the first 1000 lines
-def fetch_data(fashion_mnist=False, mnist=False, testing=False):
+def fetch_data(fashion_mnist=True, mnist=True, testing=False):
 
-    num_classes = 10
-    
     # get only fashion mnit 
     if fashion_mnist:
     
@@ -60,8 +53,6 @@ def fetch_data(fashion_mnist=False, mnist=False, testing=False):
    
     # get both mnist and fashion mnist
     if mnist and fashion_mnist: 
-
-        num_classes = 20
         
         # concatenate fashion mnist and mnist together
         X_train = np.concatenate((m_train_X, f_train_X), axis=0)
@@ -70,8 +61,8 @@ def fetch_data(fashion_mnist=False, mnist=False, testing=False):
         Y_test = np.concatenate((m_test_Y, f_test_Y), axis=0)
 
     # convert to categorical one hot vectors
-    Y_train = to_categorical(Y_train,num_classes=num_classes)
-    Y_test = to_categorical(Y_test,num_classes=num_classes)
+    Y_train = to_categorical(Y_train)
+    Y_test = to_categorical(Y_test)
     
     return X_train, Y_train, X_test, Y_test
 
@@ -92,6 +83,7 @@ def load_file(filepath, testing):
     
     # reshape the pixel data into 28x28x1
     X = np.reshape(pixel_data, (-1, IMAGE_SIZE, IMAGE_SIZE, 1))
+    
     # extract labels from the meta data
     Y = meta_data[:,0:1]
 
@@ -108,108 +100,59 @@ def lr_scheduler(epoch):
     
     return 1e-3
 
-# outlined this function to compile all models with the same parameter
-# TODO may need to modify according to the paper
-def compile_model(model):
-    model.compile(Adam(lr = LEARNING_RATE), loss = 'categorical_crossentropy', metrics=['accuracy'])
-    
+
 def create_CNN_model():
     
     # initialize a sequential model
     model = Sequential()
     
     # build the architecture
-    model.add(Conv2D(96, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1], strides = 1, activation ='relu'))
-    model.add(Conv2D(96, (3, 3), strides = 1, activation = 'relu'))
-    model.add(Conv2D(96, (3, 3), strides = 2, activation = 'relu'))
+    model.add(Conv2D(96, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1] , strides = 1, activation ='relu'))
+    model.add(Conv2D(96, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1], strides = 1, activation = 'relu'))
+    model.add(Conv2D(96, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1], strides = 2, activation = 'relu'))
     
-    model.add(Conv2D(192, (3, 3) , strides = 1, activation ='relu'))
-    model.add(Conv2D(192, (3, 3), strides = 1, activation = 'relu'))
-    model.add(Conv2D(192, (3, 3), strides = 2, activation = 'relu'))
+    model.add(Conv2D(192, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1] , strides = 1, activation ='relu'))
+    model.add(Conv2D(192, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1], strides = 1, activation = 'relu'))
+    model.add(Conv2D(192, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1], strides = 2, activation = 'relu'))
     
-    model.add(Conv2D(192, (3, 3) , strides = 1, activation ='relu'))
-    model.add(Conv2D(192, (1, 1), strides = 1, activation = 'relu'))
-    model.add(Conv2D(20, (1, 1), strides = 2, activation = 'relu'))
+    model.add(Conv2D(192, (3, 3), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1] , strides = 1, activation ='relu'))
+    model.add(Conv2D(192, (1, 1), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1], strides = 1, activation = 'relu'))
+    model.add(Conv2D(10, (1, 1), input_shape = [IMAGE_SIZE, IMAGE_SIZE, 1], strides = 2, activation = 'relu'))
     
     # Flatten
     model.add(Flatten())
     
     # add the final output softmax layer
-    model.add(Dense(20, activation ='softmax'))
+    model.add(Dense(10, activation ='softmax'))
     
     # set the model parameters
-    compile_model(model)
+    model.compile(Adam(lr = LEARNING_RATE), loss = 'categorical_crossentropy', metrics=['accuracy'])
     
     return model
 
-def save_pretrained_model(multitask_model):
-    # save the model
-    json_file = open(serialized_multitask_model_path, "w")
-    model_json = multitask_model.to_json()
-    json_file.write(model_json)
-    json_file.close()
-    # save the pre-trained weights
-    multitask_model.save_weights(serialized_multitask_weights_path)
-
-def load_pretrained_model_and_modify():
-    json_file = open(serialized_multitask_model_path, "r")
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-
-    loaded_model.load_weights(serialized_multitask_weights_path)
-
-    # remove the softmax layer on top (20 outputs)
-    loaded_model.pop()
-    # fix all the conv layers (and only train the fully connected layer on top)
-    # TODO should we consider fix-train-unfix-train?
-    for layer in loaded_model.layers:
-            layer.trainable = False
-    # add a 10 output softmax layer in place of the removed layer
-    loaded_model.add(Dense(10, activation ='softmax'))
-    compile_model(loaded_model)
-
-    return loaded_model
 
 def main():
-    all_testing_data = False
-
+    
     # fetch the data - MNIST only 
-    X_train, Y_train, X_test, Y_test = fetch_data(mnist = True, fashion_mnist = True, testing = all_testing_data)
+    X_train, Y_train, X_test, Y_test = fetch_data(fashion_mnist = False)
 
+    # # fetch the data - fashion MNIST only 
     # X_train, Y_train, X_test, Y_test = fetch_data(mnist = False)
 
-    # build the multitask_model 
-    multitask_model = create_CNN_model()
+    # build the model 
+    model = create_CNN_model()
 
     # callback for annealing the learning rate after 25 epochs
     callback = LearningRateScheduler(lr_scheduler)
-    
+
     # run training
-    multitask_model.fit(X_train, Y_train, 
+    model.fit(X_train, Y_train, 
             validation_data = (X_test, Y_test),
             epochs = EPOCHS, 
             batch_size = BATCH_SIZE, 
             callbacks=[callback],
             verbose = 2)
 
-    # save the model to disc, path is specified in serialized_multitask*
-    save_pretrained_model(multitask_model)
-    # load model for mnist
-    mn_model = load_pretrained_model_and_modify()
-
-    # train the single task model ( ONLY the top layer can be trained )
-    X_train, Y_train, X_test, Y_test = fetch_data(mnist = True, testing = all_testing_data)
-    mn_model.fit(X_train, Y_train, 
-            validation_data = (X_test, Y_test),
-            epochs = EPOCHS, 
-            batch_size = BATCH_SIZE, 
-            callbacks=[callback],
-            verbose = 2)
-
-    # similar for fasion model:
-    fa_model = load_pretrained_model_and_modify()
-    # TODO train..
 
 
 main()
